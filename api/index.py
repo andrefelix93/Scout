02d0e97e8cc7291219f33b58da6c475c1be451e9
André Felix <andre_felix1993@hotmail.com>
2025-05-09 10:58:18 -0300

    index.py

--- api/index.py
+++ api/index.py
@@ -6,6 +6,6 @@ from django.core.asgi import get_asgi_application
 
 # Set up Django project path and settings
 sys.path.append(os.path.dirname(os.path.abspath(__file__)) + '/../')
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'manager.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'ProjetoScout.settings')
 
 app = get_asgi_application()
